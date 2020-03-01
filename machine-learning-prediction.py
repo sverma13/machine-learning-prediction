d1f19ca0f433b27a2e2deae1391a30d748b61676
sverma13 <57202681+sverma13@users.noreply.github.com>
2020-03-01 14:30:44 -0600

    Update machine-learning-prediction.py

--- machine-learning-prediction.py
+++ machine-learning-prediction.py
@@ -19,7 +19,7 @@ from keras.layers import Dropout
 from sklearn.metrics import r2_score
 
 # Load stock data
-ticker = 'AAPL'
+ticker = 'MSFT'
 tickerFile = ticker + '.csv'
 stockData = pd.read_csv(tickerFile, parse_dates=['Date'])
 stockData = stockData.sort_values(by='Date')
@@ -30,17 +30,21 @@ stockData.set_index('Date', inplace=True)
 #stockData['Returns'] = returns
 
 stockData['10 Day Close Pct'] = stockData['Adj Close'].pct_change(10)
+shift = -10  # used to shift historical data for ML purposes
 
 # Define technical indicators
 rsi14 = ta.momentum.rsi(close=stockData['Adj Close'], n=14, fillna=False)
 stockData['RSI_14'] = rsi14
+bb = ta.volatility.BollingerBands(close=stockData['Adj Close'], n=20, ndev=2)
+stockData['bb_bbm'] = bb.bollinger_mavg()
+stockData['bb_bbh'] = bb.bollinger_hband()
+stockData['bb_bbl'] = bb.bollinger_lband()
 
 # Define machine learning features & targets
-shift = -10
-
 stockData = stockData.dropna()
-
-features = stockData[['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', '10 Day Close Pct', 'RSI_14']]
+featureNames = ['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume',
+                '10 Day Close Pct', 'RSI_14', 'bb_bbm', 'bb_bbh', 'bb_bbl']
+features = stockData[featureNames]
 features = features.iloc[:shift]
 targets = stockData['10 Day Close Pct'].shift(shift)
 targets = targets.iloc[:shift]
@@ -51,10 +55,6 @@ trainTargets = targets[:trainSize]
 testFeatures = features[trainSize:]
 testTargets = targets[trainSize:]
 
-
-
-
-
 # Neural network setup
 decisionTree = DecisionTreeRegressor(max_depth=5)
 decisionTree.fit(trainFeatures, trainTargets)
@@ -64,8 +64,8 @@ testPredictions = decisionTree.predict(testFeatures)
 
 treeTrain = decisionTree.score(trainFeatures, trainTargets)
 treeTest = decisionTree.score(testFeatures, testTargets)
-print('Neural network train score:', treeTrain)
-print('Neural network test score:', treeTest)
+print('Neural network train score: %0.4f' % treeTrain)
+print('Neural network test score: %0.4f' % treeTest)
 
 plt.figure()
 plt.scatter(trainPredictions, trainTargets, label='Train')
@@ -76,10 +76,6 @@ plt.ylabel('Targets')
 plt.legend()
 plt.show()
 
-
-
-
-
 # Random forest
 randomForest = RandomForestRegressor(n_estimators=200, max_depth=5)
 randomForest.fit(trainFeatures, trainTargets)
@@ -88,9 +84,16 @@ testPredictions = randomForest.predict(testFeatures)
 
 forestTrain = randomForest.score(trainFeatures, trainTargets)
 forestTest = randomForest.score(testFeatures, testTargets)
-print('Random forest train score:', forestTrain)
-print('Random forest test score:', forestTest)
-#print('Feature importance array:', randomForest.feature_importances_)
+print('Random forest train score: %0.4f' % forestTrain)
+print('Random forest test score: %0.4f' % forestTest)
+
+importances = randomForest.feature_importances_
+plt.figure()
+x = range(len(importances))
+plt.bar(x, importances, tick_label=featureNames)
+plt.xticks(rotation=90)
+plt.show()
+print('Feature importance array:', importances)
 
 plt.figure()
 plt.scatter(trainPredictions, trainTargets, label='Train')
@@ -101,21 +104,18 @@ plt.ylabel('Targets')
 plt.legend()
 plt.show()
 
-
-
-
-
-
 # Gradient boosting
-gbr = GradientBoostingRegressor(learning_rate=0.01, n_estimators=200, subsample=0.6)
+gbr = GradientBoostingRegressor(learning_rate=0.01,
+                                n_estimators=200,
+                                subsample=0.6)
 gbr.fit(trainFeatures, trainTargets)
 trainPredictions = gbr.predict(trainFeatures)
 testPredictions = gbr.predict(testFeatures)
 
 gbrTrain = gbr.score(trainFeatures, trainTargets)
 gbrTest = gbr.score(testFeatures, testTargets)
-print('Gradient boosting train score:', gbrTrain)
-print('Gradient boosting test score:', gbrTest)
+print('Gradient boosting train score: %0.4f' % gbrTrain)
+print('Gradient boosting test score: %0.4f' % gbrTest)
 
 plt.figure()
 plt.scatter(trainPredictions, trainTargets, label='Train')
@@ -132,29 +132,26 @@ sc = StandardScaler()
 scaledTrainFeatures = sc.fit_transform(trainFeatures)
 scaledTestFeatures = sc.transform(testFeatures)
 
-gbr = GradientBoostingRegressor(learning_rate=0.01, n_estimators=200, subsample=0.6)
+gbr = GradientBoostingRegressor(learning_rate=0.01,
+                                n_estimators=200,
+                                subsample=0.6)
 gbr.fit(scaledTrainFeatures, trainTargets)
 trainPredictions = gbr.predict(scaledTrainFeatures)
 testPredictions = gbr.predict(scaledTestFeatures)
 
 gbrTrain = gbr.score(scaledTrainFeatures, trainTargets)
 gbrTest = gbr.score(scaledTestFeatures, testTargets)
-print('SCALED Gradient boosting train score:', gbrTrain)
-print('SCALED Gradient boosting test score:', gbrTest)
+print('Scaled gradient boosting train score: %0.4f' % gbrTrain)
+print('Scaled gradient boosting test score: %0.4f' % gbrTest)
 
 plt.figure()
 plt.scatter(trainPredictions, trainTargets, label='Train')
 plt.scatter(testPredictions, testTargets, label='Test')
-plt.title('SCALED Gradient Boosting - Train vs Test Data')
+plt.title('Scaled Gradient Boosting - Train vs Test Data')
 plt.xlabel('Predictions')
 plt.ylabel('Targets')
 plt.legend()
 plt.show()
-
-
-
-
-
 
 """
 plt.subplot(2,1,1)
@@ -163,10 +160,10 @@ plt.subplot(2,1,2)
 plt.hist(scaledTrainFeatures, bins=5)
 plt.show()
 """
-# Keras on scaled data (originally 50 relu, 10 relu, 1 linear)
+
+# Keras on scaled data
 model = Sequential()
 model.add(Dense(50, input_dim=scaledTrainFeatures.shape[1], activation='relu'))
-#model.add(Dropout(0.5))
 model.add(Dense(10, activation='relu'))
 model.add(Dense(1, activation='linear'))
 model.compile(optimizer='adam', loss='mse')
@@ -177,14 +174,13 @@ plt.plot(history.history['loss'])
 plt.title('loss:' + str(round(history.history['loss'][-1], 6)))
 plt.show()
 
-
 trainPredictions = model.predict(scaledTrainFeatures)
 testPredictions = model.predict(scaledTestFeatures)
 
 kerasTrain = r2_score(trainTargets, trainPredictions)
 kerasTest = r2_score(testTargets, testPredictions)
-print('SCALED Keras train score:', kerasTrain)
-print('SCALED Keras test score:', kerasTest)
+print('Scaled Keras train score: %0.4f' % kerasTrain)
+print('Scaled Keras test score: %0.4f' % kerasTest)
 
 plt.figure()
 plt.scatter(trainPredictions, trainTargets, label='Train')
